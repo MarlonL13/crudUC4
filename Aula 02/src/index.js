e8b3b102b4717a7374cfa86cd63fdeebedd79f44
Valtemir Procopio <i3831@rn.senac.local>
2025-02-18 21:08:38 -0300

    fix: ajustes no index e view de aluno

--- Aula 02/src/index.js
+++ Aula 02/src/index.js
@@ -1,15 +1,49 @@
-import  {AlunoController}  from './modulos/Aluno/controllers/index.js'
+import prompt from 'prompt-sync';
+import { AlunoView } from './modulos/Aluno/views/index';
 
+const alunoView = new AlunoView();
 
-const aluno = new AlunoController()
+const input = prompt();
 
-aluno.criar("a9111","Irineu","ireneu@edum.senac.br","@123")
-aluno.criar("a9222","Borges","borges@edum.senac.br","@123")
+function exibirMenu() {
+    console.log('\n######### MENU #############');
+    console.log('1 - Criar aluno');
+    console.log('2 - Listar todos os alunos');
+    console.log('3 - Deletar aluno por matrícula');
+    console.log('4 - Deletar todos');
+    console.log('5 - Editar aluno');
+    console.log('0 - Sair');
+    console.log('##############################');
+}
 
-aluno.listarTodos()
+function main() {
+    let controle;
+    do {
+        exibirMenu();
+        controle = input('Escolha uma opção: ');
+        switch (controle) {
+            case '1':
+                alunoView.criarAluno()
+                break;
+            case '2':
+                alunoView.listarAlunos()
+                break;
+            case '3':
+                alunoView.excluirPorMatricula()
+                break;
+            case '4':
+                alunoView.excluirTodos()
+                break;
+            case '5':
+                alunoView.editarAluno()
+                break;
+            case '0':
+                console.log('Saindo...');
+                break;
+            default:
+                console.log('Opção invalida, digitte um numero do menu!');
+        }
+    } while (controle !== '0');
+}
 
-aluno.deletarPorMatricula("a9111")
-aluno.listarTodos()
-
-aluno.editar('a9222',null,"borgeseditado@edum.senac.br",null)
-aluno.listarTodos()
+main();
